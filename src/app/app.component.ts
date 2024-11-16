ce592ebd165822ff52ac2461a9607036255e5fe1
AJEversdijk <ajeversdijk@gmail.com>
2024-11-16 14:53:00 +0100

    FV: some initial work on getting the API working. Added the model, a basic service and a fetch call. still WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ChuckNorrisService } from '../services/chuck-norris/chuck-norris.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'techAssesment1';
+
+  private factServer = inject(ChuckNorrisService);
+
+  ngOnInit(): void {}
+  printFacts = () => {
+    console.log(this.factServer.chuckNorrisFacts());
+  };
 }
--- src/services/chuck-norris/chuck-norris.service.ts
+++ src/services/chuck-norris/chuck-norris.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DestroyRef, inject, Injectable, OnInit, signal } from '@angular/core';
+import { ChuckNorrisFact } from './service.model';
+import { map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ChuckNorrisService {
+export class ChuckNorrisService implements OnInit {
+  chuckNorrisFacts = signal<any>(undefined);
+  isFetching = signal<Boolean>(false);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
 
-  constructor() { }
+  ngOnInit(): void {
+    this.getChuckNorrisFacts();
+  }
+
+  getChuckNorrisFacts = () => {
+    this.isFetching.set(true);
+    const chuckNorrisFactsSubscription = this.httpClient
+      .get('https://api.chucknorris.io/jokes/random')
+      .subscribe({
+        next: (res) => {
+          this.chuckNorrisFacts.set(res);
+          console.log(res);
+        },
+        complete: () => this.isFetching.set(false),
+        error: (res) => console.log('call failed', res?.error),
+      });
+    this.destroyRef.onDestroy(() => {
+      chuckNorrisFactsSubscription.unsubscribe();
+    });
+  };
 }
