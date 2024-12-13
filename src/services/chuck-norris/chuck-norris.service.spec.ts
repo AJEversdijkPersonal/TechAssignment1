8c8b562aa523466d71a53bb2b282c0e109ff4421
AJEversdijk <ajeversdijk@gmail.com>
2024-12-13 18:49:57 +0100

    FV: add testing providers to fix inital test

--- src/services/chuck-norris/chuck-norris.service.spec.ts
+++ src/services/chuck-norris/chuck-norris.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ChuckNorrisService } from './chuck-norris.service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ChuckNorrisService', () => {
   let service: ChuckNorrisService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient(), provideHttpClientTesting()],
+    });
     service = TestBed.inject(ChuckNorrisService);
   });
 
