53317190353057fd6d510b69c02ad2fed9466110
AJEversdijk <ajeversdijk@gmail.com>
2024-12-13 18:32:09 +0100

    FV: Initial  working api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChuckNorrisService } from '../services/chuck-norris/chuck-norris.service';
 
@@ -11,10 +11,35 @@ import { ChuckNorrisService } from '../services/chuck-norris/chuck-norris.servic
 })
 export class AppComponent implements OnInit {
   title = 'techAssesment1';
+  isFetching = signal(false);
+  error = signal<string>('');
+  fact = signal<string>('');
+  facts = signal<string[]>([]);
+
+  private destroyRef = inject(DestroyRef);
 
   private factServer = inject(ChuckNorrisService);
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const chuckNorrisSub = this.factServer.getChuckNorrisFacts().subscribe({
+      next: (res) => {
+        console.log(res);
+        // this.fact.set(re);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+      error: (error) => {
+        console.log(error);
+        this.error.set(error.message);
+      },
+    });
+
+    this.destroyRef.onDestroy(() => {
+      chuckNorrisSub.unsubscribe();
+    });
+  }
   printFacts = () => {
     console.log(this.factServer.chuckNorrisFacts());
   };
--- src/services/chuck-norris/chuck-norris.service.ts
+++ src/services/chuck-norris/chuck-norris.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { DestroyRef, inject, Injectable, OnInit, signal } from '@angular/core';
-import { ChuckNorrisFact } from './service.model';
-import { map } from 'rxjs';
+import { inject, Injectable, OnInit, signal } from '@angular/core';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +9,6 @@ export class ChuckNorrisService implements OnInit {
   chuckNorrisFacts = signal<any>(undefined);
   isFetching = signal<Boolean>(false);
   private httpClient = inject(HttpClient);
-  private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     this.getChuckNorrisFacts();
@@ -18,18 +16,12 @@ export class ChuckNorrisService implements OnInit {
 
   getChuckNorrisFacts = () => {
     this.isFetching.set(true);
-    const chuckNorrisFactsSubscription = this.httpClient
-      .get('https://api.chucknorris.io/jokes/random')
-      .subscribe({
-        next: (res) => {
-          this.chuckNorrisFacts.set(res);
-          console.log(res);
-        },
-        complete: () => this.isFetching.set(false),
-        error: (res) => console.log('call failed', res?.error),
-      });
-    this.destroyRef.onDestroy(() => {
-      chuckNorrisFactsSubscription.unsubscribe();
-    });
+    return this.httpClient.get('https://api.chucknorris.io/jokes/random').pipe(
+      map((res) => res),
+      catchError((error) => {
+        console.log(error);
+        return throwError(() => new Error('something went wrong in the api'));
+      })
+    );
   };
 }
